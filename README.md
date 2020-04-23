02b15290b6947f429caaa0dd47476417eef5f4af
n1ru4l <laurinquast@googlemail.com>
2020-04-23 08:49:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# JSON Deck Scraper API for https://limitlesstcg.com/
+
+## Why
+
+[Tabletop Simulator](https://store.steampowered.com/app/286160/Tabletop_Simulator/) allows LUA scripting and I wanted to build a script for importing Pokemon Decks from the internet without all the manual work. The LUA engine is not powerful enough for parsing big HTML sites (at least the libraries I tried using).
+Because of this, I created this proxy middleware that will return a JSON response that can easily be parsed by Tabletop Simulator.
+
+The script for the Tabletop Simulator can be found here: `tabletop-simulator-script/Global.ttslua`, it uses [Decker](https://github.com/tjakubo2/Decker), which makes generating card decks easier.
+
+## Install
+
+`yarn install`
+
+## Start
+
+`node src/index.js`
+
+## API Endpoints
+
+### `/tournament/:tournamentId/:ranking`: `JSONResponse<DeckList>`
+
+**Sample Deck Link:** `https://limitlesstcg.com/tournaments/?id=261` and `5. Rank`
+
+**Endpoint for retriving deck:** `http://localhost:7777/tournament/261/5`
+
+### `/deck-list/:deckListId`: `JSONResponse<DeckList>`
+
+**Sample Deck Link:** https://limitlesstcg.com/decks/?list=3692
+
+**Endpoint for retriving deck:** `http://localhost:7777/deck-list/3692`
+
+## API Types
+
+```ts
+type JSONResponse<Body> =
+  | {
+      data: null;
+      error: {
+        code: string;
+      };
+    }
+  | {
+      data: Body;
+      err: Error;
+    };
+
+type DeckList = {
+  deckList: {
+    title: string;
+    cards: Array<{
+      amount: number;
+      url: string;
+    }>;
+  };
+};
+```
---
+++ src/index.js
@@ -0,0 +1,176 @@
+"use strict";
+
+const fetch = require("node-fetch");
+const express = require("express");
+const $ = require("cheerio");
+
+const BASE_URL = "https://limitlesstcg.com";
+
+const deckCache = new Map();
+const cardCache = new Map();
+
+const scrapeCardImageUrl = async (url) => {
+  const html = await fetch(url).then((res) => res.text());
+  return $("img.card-picture.card-image-img", html).attr("src");
+};
+
+const scrapeDeck = async (url, place) => {
+  const html = await fetch(url).then((res) => res.text());
+  const cardList = [];
+  const title = $(".decklist-top-title", html)[0].children[0].data;
+
+  $(
+    `.decklist-column:not(.energy-column-mobile) p[id^='deck']`,
+    $(`.below-filter-panel .decklist`, html)[place - 1]
+  ).each((index, e) => {
+    const cardAmount = parseInt($(".decklist-card-count", e).text(), 10);
+    const cardUrl = $("a", e).attr("href");
+    cardList.push({
+      count: cardAmount,
+      url: cardUrl,
+    });
+  });
+
+  const cards = [];
+
+  for (const { count, url } of cardList) {
+    let cardImageUrl = cardCache.get(url);
+    if (!cardImageUrl) {
+      cardImageUrl = await scrapeCardImageUrl(BASE_URL + url);
+      cardCache.set(url, cardImageUrl);
+    }
+    cards.push({
+      amount: count,
+      url: cardImageUrl,
+    });
+  }
+
+  return {
+    title,
+    cards,
+    totalCardAmount: cards.reduce((current, card) => current + card.amount, 0),
+  };
+};
+
+const buildTournamentCacheKey = (id, place) => `${id}_${place}`;
+
+const app = express();
+
+const buildTournamentScrapeUrl = (id) =>
+  `https://limitlesstcg.com/tournaments/?id=${id}&show=lists&lang=de&mode=default&view=regular`;
+
+app.get("/tournament/:id/:place", (req, res) => {
+  const id = req.params.id;
+  const place = parseInt(req.params.place, 10);
+  const cacheKey = buildTournamentCacheKey(id, place);
+
+  return (deckCache.has(cacheKey)
+    ? Promise.resolve(deckCache.get(cacheKey))
+    : scrapeDeck(buildTournamentScrapeUrl(id), place).then((deckList) => {
+        deckCache.set(cacheKey, deckList);
+        return deckList;
+      })
+  )
+    .then((deckList) => {
+      res.json({
+        data: {
+          deckList,
+        },
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        data: null,
+        error: {
+          code: "UNEXPECTED_ERROR",
+        },
+      });
+    });
+});
+
+const scrapeDeckList = async (url) => {
+  const html = await fetch(url).then((res) => res.text());
+  const $tree = $(html);
+  const title = $(".decklist-top-title", $tree)[0].children[0].data;
+  const cardList = [];
+
+  $(`.decklist-column:not(.energy-column-mobile) p[id^='deck0']`, $tree).each(
+    (i, element) => {
+      const cardAmount = parseInt(
+        $(".decklist-card-count", element).text(),
+        10
+      );
+      const cardUrl = $("a", element).attr("href");
+      cardList.push({
+        count: cardAmount,
+        url: cardUrl,
+      });
+    }
+  );
+
+  const cards = [];
+
+  for (const { count, url } of cardList) {
+    let cardImageUrl = cardCache.get(url);
+    if (!cardImageUrl) {
+      cardImageUrl = await scrapeCardImageUrl(BASE_URL + url);
+      cardCache.set(url, cardImageUrl);
+    }
+    cards.push({
+      amount: count,
+      url: cardImageUrl,
+    });
+  }
+
+  return {
+    title,
+    cards,
+    totalCardAmount: cards.reduce((current, card) => current + card.amount, 0),
+  };
+};
+
+const buildDeckListCacheKey = (id) => `deck-list_${id}`;
+const buildDeckListScrapeUrl = (id) =>
+  `https://limitlesstcg.com/decks/?list=${id}`;
+
+app.get("/deck-list/:id", (req, res) => {
+  const id = req.params.id;
+  const cacheKey = buildDeckListCacheKey(id);
+  const url = buildDeckListScrapeUrl(id);
+
+  return (deckCache.has(cacheKey)
+    ? Promise.resolve(deckCache.get(cacheKey))
+    : scrapeDeckList(url).then((deckList) => {
+        deckCache.set(cacheKey, deckList);
+        return deckList;
+      })
+  )
+    .then((deckList) => {
+      res.json({
+        data: {
+          deckList,
+        },
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        data: null,
+        error: {
+          code: "UNEXPECTED_ERROR",
+        },
+      });
+    });
+});
+
+const PORT = parseInt(process.env.PORT || "7777", 10);
+
+const server = app.listen(PORT, () => {
+  console.log(`listening on 0.0.0.0:${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  console.log(`shutting down`);
+  server.close();
+});
